e61de676921a869af9f32481e125ab5eb92c9f2e
Ilanbs14 <luisilonald14@gmail.com>
2025-06-21 23:32:55 +0200

    update particules

--- javascript/particles-config.js
+++ javascript/particles-config.js
@@ -1,61 +1,57 @@
-// particles-config.js
 tsParticles.load("particles-js", {
-  particles: {
-    number: {
-      value: 100,  // Nombre de particules
-      density: {
-        enable: true,
-        value_area: 800  // Zone d'affichage des particules
-      }
-    },
-    shape: {
-      type: "circle",  // Forme des particules
-    },
-    opacity: {
-      value: 0.5,  // Opacité des particules
-      random: true,
-      animation: {
-        enable: true,
-        speed: 1,
-        minimumValue: 0.1
-      }
-    },
-    size: {
-      value: 3,  // Taille des particules
-      random: true,
-      animation: {
-        enable: true,
-        speed: 4,
-        minimumValue: 0.1
-      }
-    },
-    links: {
-      enable: true,
-      distance: 150,
-      opacity: 0.5,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 2,
-      direction: "none",
-      random: true,
-      straight: false,
-      outModes: {
-        default: "out"
-      }
-    }
-  },
-  interactivity: {
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse"  // Effet de répulsion des particules au survol
-      },
-      onclick: {
-        enable: true,
-        mode: "push"  // Effet de "push" au clic
-      }
-    }
-  }
-});
+        particles: {
+            number: {
+                value: 60,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#26ecd1"
+            },
+            shape: {
+                type: "circle"
+            },
+            opacity: {
+                value: 0.5
+            },
+            size: {
+                value: 3
+            },
+            move: {
+                enable: true,
+                speed: 1,
+                direction: "none",
+                out_mode: "out"
+            }
+        },
+        interactivity: {
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse"
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push"
+                }
+            },
+            modes: {
+                repulse: {
+                    distance: 100
+                },
+                push: {
+                    quantity: 4
+                }
+            }
+        },
+        background: {
+            color: {
+                value: "#1a4498" // couleur de fond du canvas si tu veux le contrôler
+            }
+        },
+        fullScreen: {
+            enable: false // important pour garder dans le div #particles-js
+        }
+    });
